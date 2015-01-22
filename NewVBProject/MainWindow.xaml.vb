63f92f29398178c69f4e57e45fad9d303e69ffa9
Mark Addison <m.addison@sussex.ac.uk>
2015-01-22 17:01:30 +0000

    Added buttons and output text

--- NewVBProject/MainWindow.xaml.vb
+++ NewVBProject/MainWindow.xaml.vb
@@ -1,3 +1,10 @@
 ﻿Class MainWindow 
 
+    Private Sub btnAddAll_Click(sender As Object, e As RoutedEventArgs) Handles btnAddAll.Click
+        txtBoxResults.Text = "Adding all..."
+    End Sub
+
+    Private Sub btnAddSword_Click(sender As Object, e As RoutedEventArgs) Handles btnAddSword.Click
+        txtBoxResults.Text = "Adding sword..."
+    End Sub
 End Class
